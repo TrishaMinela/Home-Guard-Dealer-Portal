278bde595c41982b7802afc2d3cd55882524c214
Trisha Sumiran <trishaminela@Trishas-MacBook-Air.local>
2026-08-28 03:53:23 +0800

    Initialize Home Guard Dealer Portal

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <h1>Home Guard Dealer Portal</h1>
+      <p>Portal foundation is ready.</p>
+    </main>
+  )
+}
+
+export default App
